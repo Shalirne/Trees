571640bddff246525ac405fc0ee6b7ce6e4ce7f2
Shalirne <134192205+Shalirne@users.noreply.github.com>
2023-08-01 22:30:00 +0500

    Add files via upload

---
+++ Trees/Program.cs
@@ -0,0 +1,62 @@
+﻿
+namespace trees
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            string pay;
+            Console.WriteLine("Введите имя сотрудника и его зарплату. Для того, чтобы выйти введите пустую строку в графе 'Имя Сотрудника'");
+            Trees? root = new Trees();
+            root = null;
+            do
+            {
+                Console.WriteLine("Введите имя сотрудника");
+                name = Console.ReadLine();
+                if (name.Length == 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Введите зарплату сотрудника ");
+                pay = Console.ReadLine();
+                if (root == null)
+                {
+                    root = new Trees()
+                    {
+                        Name = name,
+                        Pay = double.Parse(pay)
+                    };
+                }
+                else
+                {
+                    root.AddNode(root, new Trees
+                    {
+                        Name = name,
+                        Pay = double.Parse(pay)
+                    });
+                }
+            }
+            while (name != "");
+            {
+                root.Traverse(root);
+                root.Search(root);
+                while (true)
+                {
+                    Console.WriteLine("Введите 0, чтобы перейти к началу программы (бинарное дерево очиститься)");
+                    Console.WriteLine("Введите 1, чтобы перейти к поиску зарплаты");
+                    var input = Console.ReadLine();
+                    if (input == "0")
+                    {
+                        Main();
+                    }
+                    else if (input == "1")
+                    {
+                        root.Search(root);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Trees/Trees.cs
@@ -0,0 +1,87 @@
+﻿
+namespace trees
+{
+    public class Trees
+    {
+        public string? Name;
+        public double Pay { get; set; }
+        public Trees? Left { get; set; }
+        public Trees? Right { get; set; }
+
+        public void Traverse(Trees root)
+        {
+            if (root.Left != null)
+            {
+                Traverse(root.Left);
+            }
+            Console.WriteLine($" Имя - {root.Name}, Зарплата - {root.Pay}");
+            if (root.Right != null)
+            {
+                Traverse(root.Right);
+            }
+        }
+
+        public Trees FindNode(Trees root, double number)
+        {
+            if (number < root.Pay)
+            {
+                if (root.Left != null)
+                {
+                    return FindNode(root.Left, number);
+                }
+                return null;
+            }
+            if (number > root.Pay)
+            {
+                if (root.Right != null)
+                {
+                    return FindNode(root.Right, number);
+                }
+                return null;
+            }
+            return root;
+        }
+
+        public void AddNode(Trees root, Trees toAdd)
+        {
+            if (toAdd.Pay < root.Pay)
+            {
+                if (root.Left != null)
+                {
+                    AddNode(root.Left, toAdd);
+                }
+                else
+                {
+                    root.Left = toAdd;
+                }
+            }
+            else
+            {
+                if (root.Right != null)
+                {
+                    AddNode(root.Right, toAdd);
+                }
+                else
+                {
+                    root.Right = toAdd;
+                }
+            }
+        }
+
+        public void Search(Trees root)
+        {
+            Console.WriteLine("Введите ЗП для поиска");
+            var input = Console.ReadLine();
+            var search = double.Parse(input);
+            var node = FindNode(root, search);
+            if (node == null)
+            {
+                Console.WriteLine("Не найдено");
+            }
+            else
+            {
+                Console.WriteLine($"Find {node.Pay}, Имя: {node.Name}");
+            }
+        }
+    }
+}
